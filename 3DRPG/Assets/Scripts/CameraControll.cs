bd702853112b67c8879ae657a86c97639711adbd
cso1999888 <cso1999888@gmail.com>
2020-08-22 20:39:31 +0800

    20200822_晚

---
+++ 3DRPG/Assets/Scripts/CameraControll.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    [Header("目標")]
+    public Transform target;
+    [Header("速度"), Range(0, 100)]
+    public float speed = 1;
+    [Header("旋轉速度"), Range(0, 100)]
+    public float turn = 1;
+    [Header("上下角度限制")]
+    public Vector2 limit = new Vector2(-30, 30);
+
+
+    // 滑鼠控制旋轉角度
+    private Quaternion rot;
+
+    //追蹤
+    private void Track()
+    {
+        Vector3 PosA = transform.position;                                           // A 點
+        Vector3 PosB = target.position;                                              // B 點
+        PosA = Vector3.Lerp(PosA, PosB, Time.deltaTime * speed);                     // A 點 = Vector3.插值(A 點，B 點，Time.deltaTime * speed);
+        transform.position = PosA;                                                   // 攝影機.座標 = A 點
+
+        //旋轉
+        rot.x += Input.GetAxis("Mouse Y") * turn;                                    //取得滑鼠上下來控制 X 角度
+        rot.y += Input.GetAxis("Mouse X") * turn;                                    //取得滑鼠左右來控制 Y 角度
+
+        rot.x = Mathf.Clamp(rot.x, limit.x, limit.y);                                //限制 x 在 -30 和 30 內
+
+        transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);                  //攝影機根物件.角度 = 歐拉(x,y,z);
+    }
+
+    private void Awake()
+    {
+        Cursor.visible = false;             //隱藏鼠標
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
---
+++ 3DRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    public float speed = 0.1f;
+    public float turn = 5;
+    private int attack = 10;
+    private int HP = 100;
+    private int MP = 50;
+    private int EXP;
+    private int Lv = 1;
+
+    private Rigidbody rig;
+    private Animator ani;
+    private Transform cam;   //攝影機根物件
+    #endregion
+
+    #region 事件
+    private void Awake()
+    {
+        //取得元件<泛型>();
+        //泛型：所有類型
+        rig = GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
+        cam = GameObject.Find("Camera_RootObject").transform;
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    #endregion
+
+    #region 方法
+    private void Move()
+    {
+        float v = -Input.GetAxis("Vertical");                                                 //前後 = WS & 上下
+        float h = -Input.GetAxis("Horizontal");                                               //左右 = AD & 左右
+        Vector3 pos = cam.forward * v + cam.right * h;                                       //移動座標 = 攝影機.前方 * 前後 + 攝影機.右方 * 左右
+        rig.MovePosition(transform.position + pos * speed);                                  //移動座標（原本座標 + 移動座標 * 速度）
+
+        ani.SetFloat("Move", Mathf.Abs(v) + Mathf.Abs(h));                                   //設定浮點數(絕對值 V 與 H)
+
+
+        if(v != 0 || h != 0)                                                                //如果 控制中
+        {
+            pos.y = 0;              //移動座標 y 不動
+            Quaternion angle = Quaternion.LookRotation(pos);                                //B 角度 = 面向（移動座標）
+            transform.rotation = Quaternion.Slerp(transform.rotation, angle, turn);         //A 角度 = 角度.插值（A 角度，B 角度，旋轉角度）
+        }
+    }
+    private void Attack()
+    {
+
+    }
+    private void Skill()
+    {
+
+    }
+    private void GetProp()
+    {
+
+    }
+    private void Hit()
+    {
+
+    }
+    private void Dead()
+    {
+
+    }
+    private void Exp()
+    {
+
+    }
+    #endregion
+}
